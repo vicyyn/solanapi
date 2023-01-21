7b9a071e7308eaafb02ab0601acc35cbe40f6e8a
vicyyn <nader_ouerdiane@hotmail.com>
2023-01-21 10:25:24 +0100

    error minor fix

--- programs/pisolana/src/errors.rs
+++ programs/pisolana/src/errors.rs
@@ -4,7 +4,7 @@ use crate::*;
 pub enum CustomError {
     #[msg("Invalid Hex Block Provided.")]
     InvalidHexBlockProvided,
-    #[msg("Number of Hex provided has to be between 1 and 8.")]
+    #[msg("Number of Hex provided has to be between 1 and 10.")]
     InvalidNumberOfHexProvided,
     #[msg("number_of_hex provided will overflow the Hex Block. Please provide a smaller number.")]
     HexBlockOverflow,
